064f65e550e31276f4ace1f76e6ab98c32ccbe89
takezo27 <119621422+takezo27@users.noreply.github.com>
2023-09-06 17:47:16 -0500

    console log ip
    Signed-off-by: takezo27 <119621422+takezo27@users.noreply.github.com>

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = process.env.PORT ?? 3000;
 app.use(express.static('public'))
 
 app.get('*', (req, res) => {
+    console.log({xForwardedFor: req.header('x-forwarded-for'), remoteAddress: req.connection.remoteAddress, ip: req.ip})
     res.redirect('/');
 })
 
